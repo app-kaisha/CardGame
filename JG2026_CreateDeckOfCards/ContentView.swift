00fcaafebf6240b21e235d7a2df0ba221130fe5e
app-kaisha <app.kaihatsusha@gmail.com>
2026-05-30 18:42:00 +0100

    Initial Commit

---
+++ JG2026_CreateDeckOfCards/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  JG2026_CreateDeckOfCards
+//
+//  Created by app-kaihatsusha on 30/05/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JG2026_CreateDeckOfCards/JG2026_CreateDeckOfCardsApp.swift
@@ -0,0 +1,18 @@
+//
+//  JG2026_CreateDeckOfCardsApp.swift
+//  JG2026_CreateDeckOfCards
+//
+//  Created by app-kaihatsusha on 30/05/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct JG2026_CreateDeckOfCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
